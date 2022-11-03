4218b5076bdc9f89e5da6761b0e414a781ada65f
mpalerme <marcellino.palerme@inrae.fr>
2022-11-03 10:25:17 +0100

    #6: login and forgot password OK

--- web_cab/authentification.py
+++ web_cab/authentification.py
@@ -6,26 +6,29 @@ Created on Thu Oct 27 11:22:18 2022
 @author: mpalerme
 """
 import json
+import re
 import gettext as gt
 import psycopg2 as pc2
 import streamlit as st
-import streamlit_authenticator as sta
-from streamlit_javascript import st_javascript as stj
 from web_cab.email import send_email
-from datetime import datetime, timedelta
+import bcrypt
+import numpy as np
+
 
 gt.bindtextdomain('web_cab', '/path/to/my/language/directory')
 gt.textdomain('web_cab')
 _ = gt.gettext
 
-class My_Authen(sta.Authenticate):
+class MyAuthen():
+    """
+    authentification
+    """
 
-    def __init__(self, credentials, cookie_name, key, expiry_day):
-        sta.Authenticate.__init__(self, credentials, cookie_name, key,
-                                  cookie_expiry_days=expiry_day)
+    def __init__(self, cursor):
+        self.cursor = cursor
 
 
-    def get_email(self, username):
+    def _get_email(self, username):
         """
         Get email of user
 
@@ -39,52 +42,225 @@ class My_Authen(sta.Authenticate):
         email: str.
 
         """
-        if username in self.credentials['usernames']:
-            return self.credentials['usernames'][username]['email']
-        else:
+        email_sql = "SELECT email FROM my_user WHERE login = %(username)s"
+        self.cursor.execute(email_sql, {'username':username})
+        email = self.cursor.fetchone()
+        if email is None:
             return False
 
-    def update_name(self, username, name):
+        return email[0]
+
+
+    def _get_pwd(self, username):
         """
-        Update name user
+        Get pwd of user
 
         Parameters
         ----------
         username : str
-            login of user.
-        name : str
-            Name user.
+            username of user.
+
+        Returns
+        -------
+        password: str.
+
+        """
+        if self._valid_username(username):
+            # Get  pwd
+            pwd_sql = """SELECT pwd FROM my_user WHERE login = %(username)s"""
+
+            self.cursor.execute(pwd_sql, {'username':username})
+            return self.cursor.fetchone()[0]
+
+        return ""
+
+    def _valid_pwd(self, username, pwd):
+        """
+
+
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+
+        """
+        return bcrypt.checkpw(pwd.encode(),
+                              self._get_pwd(username).encode())
+
+    def login(self) -> tuple:
+        """
+        Creates a login widget.
+        Parameters
+        ----------
+        form_name: str
+            The rendered name of the login form.
+        Returns
+        -------
+        str
+            Name of the authenticated user.
+        int
+            The status of authentication, None: no credentials entered,
+            True: correct credentials, False: incorrect credentials,
+        str
+            Username of the authenticated user.
+        """
+        st.session_state['miss'] = False
+        if ('authentication_status' not in st.session_state or
+            st.session_state.authentication_status is not True):
+            st.session_state['authentication_status'] = False
+            login_form = st.form('Login')
+
+            login_form.subheader(_('title_form_login'))
+            login_form.text_input(_('form_login_username'), key='username')
+            pwd = login_form.text_input(_('form_login_pwd'), type='password')
+
+            if login_form.form_submit_button('Login'):
+                ### Verify input not empty
+                # Thx https://stackoverflow.com/a/5063991
+                regex_no_empty = r'[^$^\ ]'
+                if(re.match(regex_no_empty, st.session_state.username) is None or
+                   re.match(regex_no_empty, pwd) is None):
+                    st.session_state.miss = True
+                else:
+                    if self._valid_pwd(st.session_state.username, pwd):
+                        st.session_state['authentication_status'] = True
+
+
+    def _update_pwd(self, username, pwd):
+        """
+
+
+        Parameters
+        ----------
+        username : TYPE
+            DESCRIPTION.
+        pwd : TYPE
+            DESCRIPTION.
 
         Returns
         -------
         None.
 
         """
-        st.session_state.name = name
-        self.exp_date = self._set_exp_date()
-        self.token = self._token_encode()
-        self.cookie_manager.set(self.cookie_name, self.token,
-                                 expires_at=datetime.now() +\
-                                      timedelta(days=self.cookie_expiry_days))
-        self._update_entry(username, 'name', name)
+        hsh_pwd = bcrypt.hashpw(pwd.encode(), bcrypt.gensalt()).decode()
 
-    def update_email(self, username, email):
+        # query for modification
+        up_pwd_sql ="UPDATE my_user SET status='temp', pwd=%(hsh_pwd)s \
+                     WHERE login=%(username)s;"
+        # Query database
+        self.cursor.execute(up_pwd_sql,
+                            {'hsh_pwd':hsh_pwd, 'username':username})
+
+
+    def _gen_pwd(self, username):
         """
 
 
         Parameters
         ----------
-        username : str
-            login of user.
-        email : str
-            New email of user.
+        username : TYPE
+            DESCRIPTION.
 
         Returns
         -------
         None.
 
         """
-        self._update_entry(username, "email", email)
+        # Define size of password
+        size_pwd = np.random.randint(12, 31)
+        type_pwd = np.dtype((str, size_pwd))
+
+        # Generate a password (32 in Ascii is Space and 126 is ~)
+        pwd = np.random.randint(low=32, high=126, size=size_pwd,
+                                dtype="int32").view(type_pwd)[0]
+
+        # save Hashed password
+        self._update_pwd(username, pwd)
+
+        return pwd
+
+    def _valid_username(self, username):
+        """
+
+
+        Parameters
+        ----------
+        username : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+        login_sql = "SELECT login FROM my_user WHERE login ~* %(start_login)s"
+        start_login = '^' + username[0]
+        self.cursor.execute(login_sql, {'start_login':start_login})
+        l_login_pwd = self.cursor.fetchall()
+        # Check if username exist
+        for user in l_login_pwd:
+            if user[0] == username:
+                # return login
+                return True
+
+        return False
+
+    def forgot_pwd(self, form_name: str) -> tuple:
+        """
+        Creates a forgot password widget.
+        Parameters
+        ----------
+        form_name: str
+            The rendered name of the forgot password form.
+
+        Returns
+        -------
+        str
+            Username associated with forgotten password.
+        str
+            Email associated with forgotten password.
+        str
+            New plain text password that should be transferred to user securely.
+        """
+
+        forgot_password_form = st.form('Forgot password')
+
+
+        forgot_password_form.subheader(form_name)
+        forgot_password_form.text_input(_('Username'),
+                                        key='v_forgot_pwd').lower()
+
+        st.session_state['state_forgot_pwd'] = 3
+        v_return = (None, None, None)
+        def inside():
+            username = st.session_state.v_forgot_pwd
+            # Thx https://stackoverflow.com/a/5063991
+            regex_no_empty = r'[^$^\ ]'
+            if not re.match(regex_no_empty, username) is None :
+                if self._valid_username(username):
+                    st.session_state['state_forgot_pwd'] = 0
+                    ### Modify user password
+                    pwd = self._gen_pwd(username)
+
+                    # Send temporary paswword by email
+                    email = self._get_email(username)
+                    send_email(dst=email, sub=_('msg_email_sub_pwd'),
+                               msg=_('msg_email_header_pwd') + pwd +
+                                   _('msg_email_end'))
+
+                else:
+                    st.session_state['state_forgot_pwd'] = 1
+            else:
+                st.session_state['state_forgot_pwd'] = 2
+
+            del st.session_state.v_forgot_pwd
+
+
+        forgot_password_form.form_submit_button(_('Submit'), on_click=inside)
+
+        return v_return
+
+
 
 def vide():
     """
@@ -95,7 +271,7 @@ def vide():
     None.
 
     """
-    pass
+
 
 def show(function):
     """
@@ -117,42 +293,6 @@ def show(function):
             return function
 
     return vide
-
-def forgot_pwd():
-    """
-    send a temporary password
-
-    Returns
-    -------
-    None.
-
-    """
-    # Generated a temporary password
-    forgot_status, email, pwd = st.session_state.authr.forgot_password(
-                                _('title_forgot_pwd'))
-
-    if forgot_status :
-        ### Modify user password
-        # query for modification
-        up_pwd_sql ='''UPDATE my_user
-                       SET status=temp
-                       SET pwd=''' + sta.Hasher(pwd) + \
-                    '''WHERE email=''' + email + ';'
-        # Query database
-        st.session_state.cursor.execute(up_pwd_sql)
-
-        # Send temporary email
-        send_email(dst=email, sub=_('msg_email_sub_pwd'),
-                   msg=_('msg_email_header_pwd') + pwd + _('msg_email_end') )
-
-        st.success(_('msg_forgot_pwd_send'))
-
-    elif forgot_status is False:
-        st.error(_('msg_forgot_pwd_fail'))
-
-    elif forgot_status is None:
-        st.info(_('msg_forgot_pwd_miss'))
-
 
 def forgot_login():
     """
@@ -180,13 +320,14 @@ def forgot_login():
                    msg=_('msg_email_header_login') + l_login[0] + \
                          _('msg_email_end'))
 
-        st.success(_("msg_forgot_login_sent"))
 
     elif forgot_status is False :
-        st.error(_('msg_forgot_login_fail'))
+        pass
 
     elif forgot_status is None :
-        st.info(_('msg_forgot_login_miss'))
+        pass
+
+    st.stop()
 
 def not_logged(function):
     """
@@ -220,32 +361,40 @@ def forgot():
     """
     a_col = st.columns(5)
     with a_col[0]:
-        st.button(_('bt_forgot_pwd'), help=_('bt_forgot_pwd_help'), on_click=
-                  forgot_pwd)
+        if st.button(_('bt_forgot_pwd'), help=_('bt_forgot_pwd_help')):
+            st.session_state.authr.forgot_pwd(_('title_forgot_pwd'))
+
     with a_col[1]:
         st.button(_('bt_forgot_login'), help=_('bt_forgot_login_help'), on_click=
                   forgot_login)
 
-
-def check_input(field):
+def init_login():
     """
 
-
-    Parameters
-    ----------
-    field : TYPE
-        DESCRIPTION.
 
     Returns
     -------
     None.
 
     """
-    result = stj('''
-                    alert(result);
-                 ''')
-    st.write(result)
-    return result
+    # Get configurations
+    with open('./web_cab/conf/conf.json', 'r', encoding='utf-8') as f_conf:
+        d_conf = json.load(f_conf)
+
+    ### Get all user information
+    # Connect to database
+    conn = pc2.connect(**d_conf['db'])
+    conn.autocommit = True
+    cursor = conn.cursor()
+    st.session_state['cursor'] = cursor
+
+    ### Manage sign in
+    # initiate login widget
+    authr = MyAuthen(cursor)
+
+    # Save obj in session
+    st.session_state['authr'] = authr
+
 
 def login(function):
     """
@@ -261,101 +410,32 @@ def login(function):
     function
 
     """
-    # Get configurations
-    with open('./web_cab/conf/conf.json', 'r', encoding='utf-8') as f_conf:
-        d_conf = json.load(f_conf)
-
-    ### Get all user information
-    # Connect to database
-    conn = pc2.connect(**d_conf['db'])
-    conn.autocommit = True
-    cursor = conn.cursor()
-    st.session_state['cursor'] = cursor
-
-    # create query
-    user_info_sql = '''SELECT login, name, email, pwd
-                       FROM my_user;'''
-
-    # query database
-    cursor.execute(user_info_sql)
-    l_user = cursor.fetchall()
-
-    # Formating dictionnary
-    d_user = {"usernames":{user[0]:{'email':user[2],
-                                    'name':user[1],
-                                    'password':user[3]}} for user in l_user}
-
-    ### Manage sign in
-    # initiate login widget
-    authr = My_Authen(d_user,d_conf['cookie']['name'], d_conf['cookie']['key'],
-                      d_conf['cookie']['expiry_day'])
-
-    # Save obj in session
-    st.session_state['authr'] = authr
+    init_login()
+    authr = st.session_state['authr']
 
     # Show widget login
-    authr.login(_('title_login'), 'main')
+    authr.login()
 
-#     stj('''var d_fied = { Username= 0, Password = 0};
+    if st.session_state.authentication_status is not True:
+        # Add buttons for forgot password or login
+        forgot()
 
-# function check(field, bt){
-#                var labels = document.getElementsByTagName('label');
-#                for(var i=0; i<labels.length; i++){
-#                   if(labels[i].textContent ==  field ){
-#                      var div = labels[i].parentElement;
-#                   }
-#                }
-#                var input = div.getElementsByTagName('input')[0];
-#                d_fied[field] = 1
-#                if (input.value == ""){
-#                   d_fied[field] = 0;
-#                 }
-#                if(d_fied.Username + d_fied.Password > 1){bt.disabled=true}
-#   else{bt.disabled=false}
-#             }
+    if 'state_forgot_pwd' in st.session_state:
+        if st.session_state.state_forgot_pwd == 1 :
+            st.error(_('msg_forgot_pwd_fail'))
+        elif st.session_state.state_forgot_pwd == 2:
+            st.info(_('msg_forgot_pwd_miss'))
+        elif st.session_state.state_forgot_pwd == 0:
+            st.info(_('msg_forgot_pwd_ok'))
+        del st.session_state.state_forgot_pwd
 
-# function add_oninput(field, bt){
-#                var labels = document.getElementsByTagName('label');
-#                for(var i=0; i<labels.length; i++){
-#                   if(labels[i].textContent ==  field ){
-#                      var div = labels[i].parentElement;
-#                   }
-#                }
-#                var input = div.getElementsByTagName('input')[0];
-#                input.oninput = check(field, bt);
-#             }
-
-# if(check('Username')==0 & check('Password')==0){
-# var bt = document.getElementsByTagName('button')
-#            for(var i=0; i<bt.length; i++){
-#               if(bt[i].textContent == 'Login'){
-#                  var res = bt[i]
-#               }
-#            }
-# res.disabled=true
-# }
-# else
-#  {
-#    res.disabled=false
-#  }
-
-# add_oninput("Username", res)
-# add_oninput("Password", res)
-
-#         ''')
-
-    # Add buttons for forgot password or login
-    forgot()
     v_return = vide
-    st.write(st.session_state.authentication_status)
-    if st.session_state.authentication_status :
+    if st.session_state.authentication_status is True :
         # add acces profile and logout on sidebar
-        authr.logout(_('bt_logout'), 'sidebar')
+        #authr.logout(_('bt_logout'), 'sidebar')
         v_return = function
+    elif st.session_state.miss is True :
+        st.info(_('msg_login_miss'))
     elif st.session_state.authentication_status is False :
-        # if int(stj('''check_empty;''')) > 0:
-        #     st.error(_('msg_login_fail'))
-        # else:
-        #     st.info(_("msg_login_miss"))
         st.error(_('msg_login_fail'))
     return v_return
