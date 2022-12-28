ec58aab26ce6f6b85984df081307cb190565c605
mpalerme <marcellino.palerme@inrae.fr>
2022-12-28 16:07:26 +0100

    #22 : plan of documentation

--- web_cab/translate.py
+++ web_cab/translate.py
@@ -8,7 +8,7 @@ Created on Thu Nov  3 15:01:29 2022
 
 import gettext as gt
 import os
-
+import re
 
 # get path of directory translate
 path_trans = os.path.join(os.path.dirname(__file__), 'msg')
@@ -17,3 +17,26 @@ lang = gt.translation('msg',
                       languages=['fr'])
 lang.install()
 _ = lang.gettext
+
+def link(txt):
+    """
+    create link in page
+
+    Parameters
+    ----------
+    txt : str
+        text.
+
+    Returns
+    -------
+    None.
+
+    """
+    # Get text translated
+    txt_trans = _(txt)
+
+    # Create link
+    # thx https://stackoverflow.com/a/8801066
+    txt_link = re.sub('[^a-zA-Z0-9]', '-', txt_trans.lower())
+
+    return '[%s](#%s)' % (txt_trans, txt_link)
