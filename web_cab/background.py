67e8e3bfb2172880828c10c326c990fca141f41d
mpalerme <marcellino.palerme@inrae.fr>
2023-01-20 07:30:58 +0100

    #28 : manage correpted zip

--- web_cab/background.py
+++ web_cab/background.py
@@ -7,6 +7,7 @@ Created on Wed Nov  9 10:52:18 2022
 
 
 """
+import zipfile
 from zipfile import ZipFile as zf
 import time
 import os
@@ -76,13 +77,30 @@ def unzip(uuid, path_temp):
     # Take name of zip
     zip_name = list(filter(lambda l_f: 'zip' in l_f, os.listdir(dir_zip)))[0]
 
+    if not zipfile.is_zipfile(os.path.join(dir_zip, zip_name)):
+        return False
+
     # create extract directory
     os.makedirs(dir_extract, exist_ok=True)
 
     # Open Zip
-    with zf(os.path.join(dir_zip, zip_name),'r') as zip_f:
-        # Extract zip
-        zip_f.extractall(path=dir_extract)
+    try:
+        zip_f = zf(os.path.join(dir_zip, zip_name),'r')
+
+    except zipfile.BadZipfile:
+        print('erreur zip')
+        return False
+
+    # Check image not upper 20Mb
+    for zp_info in zip_f.filelist:
+        if zp_info.file_size > 20 * 1024 *1024:
+            return False
+
+    # Extract zip
+    zip_f.extractall(path=dir_extract)
+
+    return True
+
 
 
 def delete_input(uuid, path_temp):
@@ -105,9 +123,9 @@ def delete_input(uuid, path_temp):
     shutil.rmtree(os.path.join(path_temp, uuid))
     shutil.rmtree(os.path.join(path_temp, uuid + '_temp'))
 
-def background(uuid, path_temp):
+def end_process(uuid, path_temp):
     """
-    processing image with cab
+    end part of processing image
 
     Parameters
     ----------
@@ -124,19 +142,6 @@ def background(uuid, path_temp):
     global g_ram_used
     global g_cpu_used
 
-    # Verify stay CPU and RAM to process
-    if (available_ressources() is False):
-        exit(1)
-
-    # Acquire ressources
-    sem.acquire()
-    g_ram_used += RAM
-    g_cpu_used += CPU
-    sem.release()
-
-    # unzip
-    unzip(uuid, path_temp)
-
     # Connect of database
     cursor = connect_dbb()
 
@@ -144,27 +149,8 @@ def background(uuid, path_temp):
     pi_sql = """UPDATE inputs SET state=%(state)s, update=CURRENT_TIMESTAMP
                 WHERE uuid=%(uuid)s;"""
 
-    # Query get option
-    option_sql = """ SELECT options FROM inputs WHERE uuid=%(uuid)s;"""
-
-    # Indicate we started process
-    cursor.execute(pi_sql, {'state':0, 'uuid':uuid})
-
-    ### Run Cab
-    # Get binary of cab
-    cab_bin = os.path.join(os.path.dirname(sys.executable), 'cab')
-    # Define path of directory with input images
-    path_in = os.path.join(path_temp, uuid,
-                           'extract')
     # Define path where save result of cab
     path_out = os.path.join(path_temp, uuid + '_temp')
-
-    # Define option of cab
-    cursor.execute(option_sql, {'uuid':uuid})
-    options = ' -l -i ' + path_in + ' -o ' + path_out + ' ' + \
-              cursor.fetchone()[0]
-    # launch cab
-    os.system(cab_bin + options)
 
     ### Zip result
     os.system(sys.executable + ' -m zipfile -c ' + os.path.join(path_temp,
@@ -201,6 +187,80 @@ def background(uuid, path_temp):
     g_ram_used -= RAM
     g_cpu_used -= CPU
     sem.release()
+
+
+def background(uuid, path_temp):
+    """
+    processing image with cab
+
+    Parameters
+    ----------
+    uuid : str
+        name of directory with zip.
+    path_temp : str
+        path where save inputs.
+
+    Returns
+    -------
+    None.
+
+    """
+    global g_ram_used
+    global g_cpu_used
+
+    # Verify stay CPU and RAM to process
+    if (available_ressources() is False):
+        exit(1)
+
+    # Acquire ressources
+    sem.acquire()
+    g_ram_used += RAM
+    g_cpu_used += CPU
+    sem.release()
+
+    # Define path where save result of cab
+    path_out = os.path.join(path_temp, uuid + '_temp')
+
+    # unzip
+    if not unzip(uuid, path_temp):
+        with open(os.path.join(path_out, 'error.txt'), 'w') as err_f:
+            print(_('msg_error_zip'),file=err_f)
+        end_process(uuid, path_temp)
+        return 5
+
+    # Connect of database
+    cursor = connect_dbb()
+
+    # Query to update database when processed image
+    pi_sql = """UPDATE inputs SET state=%(state)s, update=CURRENT_TIMESTAMP
+                WHERE uuid=%(uuid)s;"""
+
+    # Query get option
+    option_sql = """ SELECT options FROM inputs WHERE uuid=%(uuid)s;"""
+
+    # Indicate we started process
+    cursor.execute(pi_sql, {'state':0, 'uuid':uuid})
+
+    ### Run Cab
+    # Get binary of cab
+    cab_bin = os.path.join(os.path.dirname(sys.executable), 'cab')
+    # Define path of directory with input images
+    path_in = os.path.join(path_temp, uuid,
+                           'extract')
+
+
+    # Define option of cab
+    cursor.execute(option_sql, {'uuid':uuid})
+    options = ' -l -i ' + path_in + ' -o ' + path_out + ' ' + \
+              cursor.fetchone()[0]
+    # launch cab
+    os.system(cab_bin + options)
+
+    end_process(uuid, path_temp)
+
+    return 0
+
+
 
 
 def launcher(path_temp):
